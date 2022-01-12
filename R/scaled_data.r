7067adb1256b2e826ffa78c2e43ea82d5c6544b3
Art Steinmetz <apsteinmetz@yahoo.com>
2022-01-12 09:43:18 -0500

    clearer plots. removed dormat code.

--- R/scaled_data.r
+++ R/scaled_data.r
@@ -12,6 +12,7 @@ us_states_raw <- COVID19::covid19(country="US",level=2,verbose=FALSE)
 
 cutoff_start <- as.Date("2020-06-15") 
 cutoff_end <- min(max(us_states_raw$date),Sys.Date())-1
+cutoff_old_max <- as.Date("2021-11-01")
 
 # # Remove tiny territories
 territories <- c("Guam", "Northern Mariana Islands","American Samoa")
@@ -31,15 +32,12 @@ us_states <- us_states_raw %>%
   group_by(state) %>%
   # smooth the data with prior 7 days
   mutate(cases_7day = (cases_total - lag(cases_total, 7)) / 7) %>%
-  mutate(deaths_7day = (deaths_total - lag(deaths_total, 7)) / 7)%>%
-  # smooth the data with a centered mean of 7 days
-  mutate(cases_1day = (cases_total - lag(cases_total, 1))) %>%
-  mutate(deaths_1day = (deaths_total - lag(deaths_total, 1)))
+  mutate(deaths_7day = (deaths_total - lag(deaths_total, 7)) / 7)
 # ----------------------------------------------
 # find max point prior to current surge
 
 get_old_max <- function(dataset,type = c("cases","deaths")){
-  dataset <- dataset %>% filter(date<as.Date("2021-11-01"))
+  dataset <- dataset %>% filter(date < cutoff_old_max)
   series <- switch(type,
                    cases = dataset$cases_7day,
                    deaths = dataset$deaths_7day)
@@ -49,11 +47,11 @@ get_old_max <- function(dataset,type = c("cases","deaths")){
 }
 
 # create lags
-lag_period = 14
+lag_period = 21
 
 # get peaks based on early waves
 state_peaks <- us_states %>% 
-  filter(date < as.Date("2021-11-01")) %>% 
+  filter(date < cutoff_old_max) %>% 
   group_by(state) %>% 
   summarise(old_max_cases=max(cases_7day,na.rm = T),
             old_max_deaths=max(deaths_7day,na.rm = T))
@@ -78,26 +76,48 @@ us_states_long <- us_states_lag %>%
 
 
 #plot scaled and lagged count for selected stated
-states = c("New York","Texas")
-us_states_long %>%
+states = c("New York")
+gg1 <- us_states_long %>%
   filter(state %in% states) %>%
+  filter(type =="scaled_deaths") %>%
   ggplot(aes(date, value, color = type)) +
   geom_line(size = 1.2) +
-  scale_color_manual(values=c("tomato2","black"),
-                     labels = c("Cases Pushed Forward 2 Weeks",
-                              "Deaths to Date")) + 
+  scale_color_manual(values=c("black"),
+                     labels = c("7-Day Avg. of Deaths on Date")) + 
   facet_grid(cols=vars(state),scales = "free") +
   scale_y_continuous(labels = scales::percent) +
   labs(
-    title = "Covid Cases and Deaths in U.S. As A Fraction of Last Year's Peak",
-    subtitle = "Cases led by 2 Weeks",
+    title = "What Does This Mean?",
     y = "Percent of Last Years Peak",
     x = "",
-    caption = "Source:covid19datahub.io,Arthur Steinmetz"
+    caption = "Source: covid19datahub.io, Arthur Steinmetz"
   ) +
   theme_light() +
   theme(legend.title = element_blank(), legend.position = "top")
 
+states = c("New York")
+gg2 <- us_states_long %>%
+  filter(state %in% states) %>%
+  ggplot(aes(date, value, color = type)) +
+  geom_line(size = 1.2) +
+  scale_color_manual(values=c("tomato2","black"),
+                     labels = c(glue::glue("Cases {lag_period} Days After Date"),
+                              "7-Day Avg. of Deaths on Date")) + 
+  facet_grid(cols=vars(state),scales = "free") +
+  scale_y_continuous(labels = scales::percent) +
+  labs(
+    title = "It Means This",
+    subtitle = glue::glue("Deaths Follow Cases.  Will The Relationship Hold?"),
+    y = "Percent of Last Years Peak",
+    x = "",
+    caption = "Source: covid19datahub.io, Arthur Steinmetz"
+  ) +
+  theme_light() +
+  theme(legend.title = element_blank(), legend.position = "top")
+
+cowplot::plot_grid(gg1,gg2,ncol = 1)
+
+# Now show National level results ----------------------
 # aggregate state to national
 us_lag <- us_states_lag %>%
   group_by(date) %>%
@@ -109,8 +129,8 @@ us_lag <- us_states_lag %>%
   mutate(deaths_7day=ifelse(deaths_7day==0,NA,deaths_7day))
 
 #scale US to % of peak
-old_max_deaths <- get_old_max(us,"deaths")
-old_max_cases <- get_old_max(us,"cases")
+old_max_deaths <- get_old_max(us_lag,"deaths")
+old_max_cases <- get_old_max(us_lag,"cases")
 us_long <- us_lag %>% 
   mutate(scaled_cases=cases_7day/old_max_cases) %>% 
   mutate(scaled_deaths=deaths_7day/old_max_deaths) %>% 
@@ -124,10 +144,10 @@ us_long %>%
   ggplot(aes(date,value,color=type)) + geom_line(size=1.2) +
   scale_y_continuous(labels = scales::percent) +
   scale_color_manual(values=c("tomato2","black"),
-                     labels = c("Cases Pushed Forward 3 Weeks",
-                                "Deaths to Date")) + 
+                     labels = c(glue::glue("Cases Pushed Forward {lag_period} Days"),
+                                "7-Day Avg. of Deaths on Date")) + 
   labs(title="Covid Cases and Deaths in U.S.",
-       subtitle = "Cases Led by 3 Weeks",
+       subtitle = glue::glue("Cases Led by {lag_period} Days"),
        y="Percent of Last Years Peak",
        x = "Date",
        caption = "Source:covid19datahub.io,Arthur Steinmetz")+
